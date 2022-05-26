65e4acc2e84e562977297c2e5dfa94f3051cbf2d
Samuel <smarouba2@gmail.com>
2022-05-26 16:08:02 -0500

    commit tree ad598299521b205e59004160f2ea6f97f91bf587

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const currentUser = 'currentUser';
+const currentUser = 'samuelMalwal';
 const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, ${c.slice(0,1)}!';
+// const shortGreeting = 'Welcome, ${c.slice(0,1)}!';
+
+
+console.log(excitedWelcomeMessage)
+console.log(welcomeMessage)
+console.log(currentUser)
+console.log(shortGreeting)
